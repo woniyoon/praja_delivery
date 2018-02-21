d8f631a4ed410509186141d4f8aaeb0ab87b80b2
woniyoon <gellifisch@gmail.com>
2018-02-20 21:52:33 -0800

    Re-write the name of enum in Address & Coupon in Lowercase

--- delivery/delivery/Models/Coupon.swift
+++ delivery/delivery/Models/Coupon.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum discountType {
+enum DiscountType {
     case cash
     case percentage
 }
@@ -20,9 +20,9 @@ class Coupon {
     public let startDate : Date
     public let endDate : Date
     public let discountValue : Int
-    public let discountType : discountType
+    public let discountType : DiscountType
     
-    init(couponId : String, description : String, code : String, startDate : Date, endDate : Date, discountValue : Int, discountType : discountType) {
+    init(couponId : String, description : String, code : String, startDate : Date, endDate : Date, discountValue : Int, discountType : DiscountType) {
         self.couponId = couponId
         self.description = description
         self.code = code
