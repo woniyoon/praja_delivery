754a77a367537525068c0abb2deacd50c03296a3
Alireza Davoodi <alireza.davoodi@gmail.com>
2018-02-20 18:57:43 -0800

    create the delivery project

---
+++ delivery/delivery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delivery
+//
+//  Created by Alireza Davoodi on 2018-02-20.
+//  Copyright © 2018 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
