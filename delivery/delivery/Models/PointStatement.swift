c841580a5a2dccaceaecb940915ba03cd240688d
blbacelar <blbacelar@gmail.com>
2018-02-21 21:35:54 -0800

    event, pointStatement, ProductImage, Review and User models created

---
+++ delivery/delivery/Models/PointStatement.swift
@@ -0,0 +1,18 @@
+//
+//  PointStatement.swift
+//  delivery
+//
+//  Created by Bacelar on 2018-02-21.
+//  Copyright © 2018 CICCC. All rights reserved.
+//
+
+import Foundation
+
+struct PointStatement {
+    
+    public let earnedPoints: Int
+    public let consumedPoints: Int
+    public let order: Int
+    public let user: User
+    
+}
