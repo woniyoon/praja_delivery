cc5545c1df9088aaa621a55c64b973ef86c6be48
woniyoon <gellifisch@gmail.com>
2018-02-21 20:36:09 -0800

    Rename the variable in Product

--- delivery/delivery/Models/Product.swift
+++ delivery/delivery/Models/Product.swift
@@ -15,16 +15,16 @@ class Product {
     private(set) public var brand: String
     private(set) public var discountPercent: Int? = nil
     private(set) public var subCategory: SubCategory
-    private(set) public var image: [String]
+    private(set) public var images: [String]
     
-    init(name: String, description: String, price: Double, brand: String, discountPercent: Int, subCategory: SubCategory, image: [String]) {
+    init(name: String, description: String, price: Double, brand: String, discountPercent: Int, subCategory: SubCategory, images: [String]) {
         self.name = name
         self.description = description
         self.price = price
         self.brand = brand
         self.discountPercent = discountPercent
         self.subCategory = subCategory
-        self.image = image
+        self.images = images
     }
     
 }
