c258c9a74649f41e9e3c28f4d89729a5cee98e49
woniyoon <gellifisch@gmail.com>
2018-04-03 22:59:08 -0700

    Create CheckoutViewController

---
+++ delivery/delivery/Presentation/Checkout/CheckoutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CheckoutViewController.swift
+//  delivery
+//
+//  Created by Jaewon Yoon on 2018-04-03.
+//  Copyright © 2018 CICCC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckoutViewController: UIViewController {
+    
+}
