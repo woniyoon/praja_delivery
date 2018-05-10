e6ff076e3109c6c9418353d5994d4acb6045e3ee
Kento Uchida <kento.apps@gmail.com>
2018-05-09 21:43:35 -0700

    Remove comment in ProductDetailFirebaseDataStore

--- delivery/delivery/Data/ProductDetail/ProductDetailFirebaseDataStore.swift
+++ delivery/delivery/Data/ProductDetail/ProductDetailFirebaseDataStore.swift
@@ -15,12 +15,9 @@ class ProductDetailFirebaseDataStore: ProductDetailDataStoreProtocol {
     
     func fetchProductDetail(_ productId: String) -> Single<ProductEntity> {
         let productRef = db.collection(PRODUCT_COLLECTION).document(productId)
-        print("========= fetch")
         return Single<ProductEntity>.create { observer -> Disposable in
-            print("========= single")
             self.db.runTransaction({ (transaction, errorPointer) -> Any? in
                 
-                print("========= transaction")
                 let productDoc: DocumentSnapshot
                 do {
                     try productDoc = transaction.getDocument(productRef)
@@ -34,20 +31,12 @@ class ProductDetailFirebaseDataStore: ProductDetailDataStoreProtocol {
                 }
                 let collection = productDoc.reference.collection("review")
                 collection.addSnapshotListener({ (querySnapshot, error) in
-                    print("========= snapshot")
                     guard let snapshot = querySnapshot else {
                         print("Error fetching snapshot results: \(error!)")
                         observer(.success(product))
                         return
                     }
                     
-//                    var reviews: [ReviewEntity] = []
-//                    for reviewDict in snapshot.documents as [Any] {
-//                        let review = ReviewEntity(dictionary: (reviewDict as? [String : Any])!)
-//                        if let review = review {
-//                            reviews.append(review)
-//                        }
-//                    }
                     let reviews = snapshot.documents
                         .map { ReviewEntity(dictionary: $0.data()) }
                         .filter { $0 != nil } as! [ReviewEntity]
@@ -55,25 +44,9 @@ class ProductDetailFirebaseDataStore: ProductDetailDataStoreProtocol {
                     observer(.success(product))
                 })
                 
-                print("========= DONE")
                 return nil
             }, completion: { (product, error) in
-                print("hey")
-                print("yo")
             })
-//            self.db.collection(PRODUCT_COLLECTION)
-//                .document(productId)
-//                .getDocument { (document, error) in
-//                    if let error = error {
-//                        observer(.error(NomnomError.network(code: "", message: ErrorMsg.tryAgain, log: error.localizedDescription)))
-//                        return
-//                    }
-//                    guard let product = ProductEntity(docId: (document?.documentID)!, dictionary: (document?.data())!) else {
-//                        observer(.error(NomnomError.alert(message: "Parse Failure")))
-//                        return
-//                    }
-//                    observer(.success(product))
-//            }
             return Disposables.create()
         }
     }
