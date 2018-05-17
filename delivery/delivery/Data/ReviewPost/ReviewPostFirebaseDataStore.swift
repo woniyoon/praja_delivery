fa55c4bc255e3ecb9a953020b02aa4f34fe7f82b
Kento Uchida <kento.apps@gmail.com>
2018-05-16 19:16:40 -0700

    Modify ReviewPost to calculate average rating

--- delivery/delivery/Data/ReviewPost/ReviewPostFirebaseDataStore.swift
+++ delivery/delivery/Data/ReviewPost/ReviewPostFirebaseDataStore.swift
@@ -15,8 +15,10 @@ import RxCocoa
 class ReviewPostFirebaseDataStore: ReviewPostDataStoreProtocol {
     
     private let db = Firestore.firestore()
+    private var reviewId: String?
     
     func fetchReview(productId: String) -> Single<ReviewEntity> {
+        self.reviewId = nil
         guard let user = Auth.auth().currentUser else {
             return Single.error(NomnomError.alert(message: "Please sign in"))
         }
@@ -37,6 +39,7 @@ class ReviewPostFirebaseDataStore: ReviewPostDataStoreProtocol {
                             observer(.error(NomnomError.alert(message: "Parse Failure")))
                             return
                         }
+                        self.reviewId = doc[0].documentID
                         observer(.success(review))
                     }
                 })
@@ -50,17 +53,48 @@ class ReviewPostFirebaseDataStore: ReviewPostDataStoreProtocol {
         }
         let review = ReviewEntity(comment: comment, rating: rating, title: title, userId: user.uid, userName: userName)
         
+        let productRef = db.collection(PRODUCT_COLLECTION).document(productId)
+        
+        let reviewRef: DocumentReference
+        if let reviewId = reviewId {
+            reviewRef = productRef.collection(REVIEW_COLLECTION).document(reviewId)
+        } else {
+            reviewRef = productRef.collection(REVIEW_COLLECTION).document()
+        }
+        
         return Completable.create { observer -> Disposable in
-            self.db.collection(PRODUCT_COLLECTION)
-                .document(productId)
-                .collection(REVIEW_COLLECTION)
-                .addDocument(data: review.dictionary) { error in
-                    if let error = error {
-                        observer(.error(error))
-                    } else {
-                        observer(.completed)
-                    }
+            self.db.runTransaction({ (transaction, errorPointer) -> Any? in
+                let productSnapshot: DocumentSnapshot
+                do {
+                    try productSnapshot = transaction.getDocument(productRef)
+                } catch let error as NSError {
+                    errorPointer?.pointee = error
+                    return nil
                 }
+                
+                guard let product = ProductEntity(docId: productSnapshot.documentID, dictionary: productSnapshot.data()!) else {
+                    let error = NSError(domain: "FireEatsErrorDomain", code: 0, userInfo: [
+                        NSLocalizedDescriptionKey: "Unable to write to restaurant at Firestore path: \(productRef.path)"
+                        ])
+                    errorPointer?.pointee = error
+                    return nil
+                }
+                
+                let newAverage = (Double(product.ratingCount) * product.averageRating + Double(review.rating)) / Double(product.ratingCount + 1)
+                
+                transaction.setData(review.dictionary, forDocument: reviewRef)
+                transaction.updateData([
+                    "ratingCount": product.ratingCount + 1,
+                    "averageRating": newAverage
+                    ], forDocument: productRef)
+                return nil
+            }, completion: { (object, error) in
+                if let error = error {
+                    observer(.error(error))
+                } else {
+                    observer(.completed)
+                }
+            })
             return Disposables.create()
         }
     }
